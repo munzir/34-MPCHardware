e07b1b8ae75676ea8dc45236ebe676b1bf679710
John Zhang <john.shimin.zhang@gmail.com>
2018-08-17 13:13:50 -0400

    add code for exiting MPC mode

--- balancing/exe/tests/02-ComputeDDPTraj.cpp
+++ balancing/exe/tests/02-ComputeDDPTraj.cpp
@@ -18,7 +18,7 @@ pthread_mutex_t g_state_mutex;
 pthread_mutex_t g_augstate_mutex;
 pthread_mutex_t g_robot_mutex;
 
-int main() {
+void main() {
 
     pthread_mutex_init(&g_state_mutex, NULL);
     pthread_mutex_init(&g_augstate_mutex, NULL);
@@ -29,8 +29,9 @@ int main() {
     Vector2d g_augstate = Vector2d::Zero();
 
     readMDPConfig();
-
+    double time_start = get_time();
     computeDDPTrajectory(threeDOF);
+    double time_end = get_time();
+    cout << "computing DDP took " << time_edd - time_start << " seconds" << endl;
 
-    return 0;
 }
